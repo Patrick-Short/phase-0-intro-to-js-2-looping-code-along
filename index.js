72a3b6062bdaa88a1820410739a096f3e7ef729f
Patrick Short <pshort0905@gmail.com>
2023-04-29 10:52:02 -0400

    Adds code for loops.

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
-// Code your solutions in this file
+// Code your solutions in this file 
+
+const names = ["Guadalupe", "Ollie", "Aki"]
+
+
+function writeCards(names) {
+    let newArray = []
+  
+      let i = 0;
+      while (i < names.length) {
+        newArray.push(`Thank you, ${names[i]}, for the wonderful surprise gift!`);
+        i++;
+        
+      
+    }
+   return newArray;
+
+}
+
+
+  let t = 10
+  
+
+  function countDown(t) {
+    while (t >= 0) {
+      console.log(t--);
+      
+    }
+  }
+
+
